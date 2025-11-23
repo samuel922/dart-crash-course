cb3d26ec9dec0f4ccc313f42975ff8feaf904717
samuel922 <onyangosamuel583@gmail.com>
2025-11-23 07:48:55 +0300

    completed example 6 - explaining type inference in dart

---
+++ variables_and_data_types/bin/example6.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  const int yourAge = 27;
+  const myAge = 25;
+  print(yourAge);
+  print(myAge);
+
+  /// This is explains that there is not need to add the actual type alongside final,const or var keyword
+  /// since dart has type inference feature;
+}
