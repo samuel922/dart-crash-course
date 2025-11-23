7f3ecbcb682dbdf9afa49c79e3ed02a446ccabac
samuel922 <onyangosamuel583@gmail.com>
2025-11-23 11:11:54 +0300

    working with operators - mathematical operators in dart

---
+++ operators/bin/example1.dart
@@ -0,0 +1,19 @@
+void main(List<String> args) {
+  /// Let's explore basic math operators
+  final num1 = 50;
+  final num2 = 23;
+
+  print('--Addition--');
+  print(num1 + num2);
+  print('--Subtraction--');
+  print(num1 - num2);
+  print('--Multiplication');
+  print(num1 * num2);
+  print('--Division--');
+  print(num1 / num2);
+  print('Truncating Division--');
+  print(num1 ~/ num2);
+  print('--Modulus operation');
+  print(num1 % num2);
+}
+
