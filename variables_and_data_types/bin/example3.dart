d55a51f3461a25d2aff369a8c6c5f6b50384a6ff
samuel922 <onyangosamuel583@gmail.com>
2025-11-23 07:00:31 +0300

    completed example 3 - creating variables with var keyword

---
+++ variables_and_data_types/bin/example3.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  var address = '243 Main St';
+  print(address);
+
+  address = '456 Main St';
+  print(address);
+
+  /// With var keyword, we can perfomr all kinds of mutations to the variable in question unlike
+  /// const and final keywords
+}
