6ca4bff3a5e68557c8d3ab38199ce03340f9e57c
samuel922 <onyangosamuel583@gmail.com>
2025-11-23 10:24:54 +0300

    completed example 8 - explicit data types in dart

---
+++ variables_and_data_types/bin/example8.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  final String name = 'Rabala';
+  print(name);
+  final int age = 25;
+  print(age);
+  final isSingle = false;
+  print(isSingle);
+  final double monthlySalary = 55000.00;
+  print(monthlySalary);
+  final List<String> myFriends = ['Joseph', 'Keith', 'Omosh'];
+  print(myFriends);
+  final Map<String, String> role = {'developer': 'Flutter Developer'};
+  print(role);
+  final Set<int> favoriteNumbers = {1,3,4,5,7};
+  print(favoriteNumbers);
+}
