acc6980ba6645f29083bde94b68fbcba02841330
samuel922 <onyangosamuel583@gmail.com>
2025-11-23 07:24:21 +0300

    completed example 5 - working with assigning final and const variable to each other

---
+++ variables_and_data_types/bin/example5.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  const age = 25;
+  final age1 = age; // This means we can assign const a final variable
+  print(age1);
+
+  //const age2 = age1; //We cannot assign a final variable to a const variable
+}
