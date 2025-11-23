72dc2e80250d2999eedd90051dd74b14095a3a54
samuel922 <onyangosamuel583@gmail.com>
2025-11-23 06:46:58 +0300

    completed first example - working with const variables

---
+++ variables_and_data_types/bin/example1.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+    const foo = 'Hello';
+    print(foo);
+
+    //With const,  there are two kinds of immutability;
+    /// 1. We cannot reassign the variable to an netirely new value,
+    /// 2. The internal content also cannot be changed.
+}
