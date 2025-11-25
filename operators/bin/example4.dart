c0ff7580a7be15482153b91baa0e6af7383ba854
samuel922 <onyangosamuel583@gmail.com>
2025-11-25 06:28:17 +0300

    completed example 4 - working with bitwise infix operators

---
+++ operators/bin/example4.dart
@@ -0,0 +1,30 @@
+void main(List<String> args) {
+  ///Working with binary infix operators
+  ///Mathematical bitwise infix operators
+  var age = 25;
+  print(age + 20); //Addition
+  print(age - 20);
+  print(age * 20);
+  print(age / 20);
+  print(age ~/ 20);
+  print(age % 20);
+
+  ///Comparison operator
+  print(age < 20);
+  print(age > 20);
+  print(age <= 20);
+  print(age >= 20);
+
+  //Assignment operators
+  print(age == 20);
+  print(age != 20);
+
+  //Logical
+  print(age & 20);
+  print(age | 20);
+  print(age ^ 20);
+
+  //SHif
+  print(age >> 20);
+  print(age << 20);
+}
