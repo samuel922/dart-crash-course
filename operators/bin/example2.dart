c05a72e1e43be3a86c1341d252451c0c3e6fd129
samuel922 <onyangosamuel583@gmail.com>
2025-11-25 06:06:26 +0300

    working with operators

---
+++ operators/bin/example2.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  ///Lets explore different types of operators in dart
+  /// 1. Unary prefix operators
+  /// 2. Unary postfix operator
+  /// 3. Binary infix
+  /// 4. compound assignment operator
+  /// 
+  /// . 1. Unary prefix - 
+  int number = 80;
+  print(--number); //Internaly changes the value
+  print(++number);
+  print(-number);
+  print(!false);
+  print(!!true);
+  print(!!false);
+  print(~3);
+}
---
+++ operators/bin/example3.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  ///We are going to work with unary postfix operators
+  ///
+  var age = 25;
+  print(age--);
+  print(age);
+  print(age++);
+  print(age);
+}
